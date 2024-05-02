61595352e438590207d52bda976ebb0bd41fd39b
salmasgh <salmasghaier74@gmail.com>
2024-05-02 13:17:37 +0100

    landing page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(SessionInterface $session): Response
     {
-        $session.set('userID',null);
-        return $this->render('home/blog.html.twig', [
+        $session->set('userID',null);
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
